14912f429adcf777b764c40e8fa227beae4facd3
chrismjohnston <cmjohnston@gmail.com>
2019-08-09 12:00:19 -0500

    cards component is complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -22,34 +22,59 @@ axios.get('https://lambda-times-backend.herokuapp.com/articles')
 
 //create success and failure case for promise with props to Twitter for error message
 
-.then(items => {
-    const javaArticles = items.data.articles.javascript;
-    const bootStrapArticles = items.data.articles.bootstrap;
-    const techArticles = items.data.articles.technology;
-    const jqueryArticles = items.data.articles.jquery;
-    const nodeArticles = items.data.articles.node;
-
+.then(data => {
+    const javaArticles = data.data.articles.javascript
+        const bootStrapArticles = data.data.articles.bootstrap;
+        const techArticles = data.data.articles.technology;
+        const jqueryArticles = data.data.articles.jquery;
+        const nodeArticles = data.data.articles.node
     //create an array for the forEach to loop over
     const articles = [javaArticles, bootStrapArticles, techArticles, jqueryArticles, nodeArticles]
 
     articles.forEach(article => {
         article.forEach(art => {
             const cards = document.querySelector('.cards-container');
-            cards.addEventListener(Article(art));
+            cards.appendChild(Article(art));
         })
-        
     })
-
-    console.log('It works, cards', items)
+    console.log('It works, cards', data);
 })
 
 .catch(error => {
     console.log("Sorry, something went wrong. A team of highly trained monkeys has been dispatched to deal with this situation", error);
 })
 
-function Article(obj){
+
+function Article(obj) {
     //created the necessary elements in the DOM for the card container
     const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
 
+
+
+    //Create classes for each card element with a class
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imageContainer.classList.add('img-container');
+
+    //sets data types for headline, image, and author name classes
+    headline.textContent = obj.headline;
+    img.src = obj.authorPhoto;
+    authorName.textContent = obj.authorName;
+
+    //Create order for elements created at top of function
+    imageContainer.appendChild(img);
+    author.appendChild(imageContainer);
+    author.appendChild(authorName);
+    card.appendChild(headline);
+    card.appendChild(author);
+
+    //returns the card so it can display in the div
     return card;
+
 }
